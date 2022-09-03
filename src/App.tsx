e3e2f1920e5058f46225b8801840e20791bf588d
unknown <a.schropp@hotmail.com>
2022-09-02 21:10:14 -0400

    Updated react app to use typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import Nav from './Components/Nav/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+    </div>
+  );
+}
+
+export default App;
