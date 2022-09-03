f52ede2896061b31ad76762a1076fb9461ea3022
unknown <a.schropp@hotmail.com>
2022-09-02 21:11:19 -0400

    Added nav bar

---
+++ src/Components/Nav/Nav.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const Nav = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  return (
+    <nav className="w-full bg-gradient-to-b from-dark-grey shadow">
+      <div className="justify-between px-4 mx-auto md:items-center md:flex md:px-8">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <a href="javascript:void(0)">
+              <h2 className="text-xl font-bold text-white">little witch logo</h2>
+            </a>
+            <div className="md:hidden">
+              <button className="p-2 text-light-grey rounded-md outline-none focus:border-gray-400 focus:border" onClick={() => setNavbar(!navbar)}>
+                {navbar ? (
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"/>
+                  </svg>
+                  ) : (
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16"/>
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"}`}>
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              <li className="text-light-grey hover:text-white">
+                  <a href="javascript:void(0)">shop</a>
+              </li>
+              <li className="text-light-grey hover:text-white">
+                  <a href="javascript:void(0)">about</a>
+              </li>
+              <li className="text-light-grey hover:text-white">
+                  <a href="javascript:void(0)">contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="hidden space-x-2 md:inline-block">
+          <a href="javascript:void(0)" className="px-4 py-2 stroke-light-grey hover:stroke-white">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
