ecbf3a004fe5a9d02bb6562f2e74f43ff0cc9e81
unknown <a.schropp@hotmail.com>
2022-09-02 21:10:52 -0400

    Added tailwind theme colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,scss,ts,tsx}',
   ],
   theme: {
+    colors: {
+      'purple': '#8D89A3',
+      'dark-grey': '#212121',
+      'light-grey': '#CBCBCF',
+      'white': '#FFFFFF',
+
+    },
     extend: {},
   },
   plugins: [],
