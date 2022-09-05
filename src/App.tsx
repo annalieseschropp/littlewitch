61884016041fb6c9d7083817ffadf57285ec2a51
unknown <a.schropp@hotmail.com>
2022-09-05 16:50:33 -0400

    Modified main app for testing, added homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Nav from './Components/Nav/Nav';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Home/>
     </div>
   );
 }
--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -4,7 +4,7 @@ const Nav = () => {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className="w-full bg-gradient-to-b from-dark-grey shadow">
+    <nav className="w-full bg-gradient-to-b from-dark-grey">
       <div className="justify-between px-4 mx-auto md:items-center md:flex md:px-8">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
