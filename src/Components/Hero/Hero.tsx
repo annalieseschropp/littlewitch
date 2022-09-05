aebd6f4466b5745527e479d1d0c02e71dbeb0844
unknown <a.schropp@hotmail.com>
2022-09-05 16:50:01 -0400

    Added base hero component

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,9 +1,19 @@
 // import { useState } from "react";
+import './Hero.scss';
+import Button from "../Button/Button";
 
 const Hero = () => {
 
   return (
-    <></>
+    <div className='hero-container'>
+      <div className="grid grid-cols-3 gap-6 h-full">
+        <div className="col-span-2 border-r-2 border-light-grey"></div>
+        <div className="hero-text">
+          <h1 className="text-white font-header text-5xl">discover beautiful <br/> handcrafted <br/> dice sets.</h1>
+          <Button primary text="shop now" onClick={() => console.log("shop now has been clicked")}/>
+        </div>
+      </div>
+    </div>
   );
 }
 
