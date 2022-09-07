990115a92af4eeea7f51481c6bc59fd931f9c295
unknown <a.schropp@hotmail.com>
2022-09-06 22:07:44 -0400

    change to banner image, changes to hero + styling, adding next section title

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className="grid grid-cols-3 gap-6 h-full">
         <div className="col-span-2 border-r-2 border-light-grey"></div>
         <div className="hero-text">
-          <h1 className="text-white font-header text-5xl">discover beautiful <br/> handcrafted <br/> dice sets.</h1>
+          <h1 className="text-white font-header text-5xl mb-4">discover beautiful <br/> handcrafted <br/> dice sets.</h1>
           <Button primary text="shop now" onClick={() => console.log("shop now has been clicked")}/>
         </div>
       </div>
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -3,9 +3,14 @@ import Hero from '../../Components/Hero/Hero';
 const Home = () => {
 
   return (
-    <>
-      <Hero/>
-    </>
+    <div className="dark-mode">
+      <Hero />
+      <div className="relative flex flex-row py-5 pr-2 items-center w-2/3">
+        <div className="basis-1/6 border-t-2 border-light-grey"></div>
+        <span className="flex-shrink mx-8 text-white text-left font-header text-4xl">shop</span>
+        <div className="basis-5/6 border-t-2 border-light-grey"></div>
+      </div>
+    </div>
   );
 }
   
